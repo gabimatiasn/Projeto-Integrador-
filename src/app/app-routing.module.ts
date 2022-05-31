3830791f0218b6ab7b39a9b93611d6845ce40ec9
julianarpaz <julianarpaz@gmail.com>
2022-05-31 19:32:48 -0300

    Adicionando rotas e modificando tamanho dos icones da navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'inicio', pathMatch: 'full'},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'cadastro', component: CadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
