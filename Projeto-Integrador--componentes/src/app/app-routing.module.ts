13c9cb45d8edb13ccbd9a9c75fdc19d7e76b3ba9
willihane <williane.pereira@outlook.com>
2022-05-31 18:28:36 -0300

    Inserindo card em login

--- Projeto-Integrador--componentes/src/app/app-routing.module.ts
+++ Projeto-Integrador--componentes/src/app/app-routing.module.ts
@@ -3,18 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 import { InicioComponent } from './inicio/inicio.component';
-import { MenuComponent } from './menu/menu.component';
-import { RodapeComponent } from './rodape/rodape.component';
 
 const routes: Routes = [
 
-  {path:'', redirectTo: 'login', pathMatch: 'full'},
+  {path:'', redirectTo: 'inicio', pathMatch: 'full'},
 
   {path: 'inicio', component: InicioComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'cadastro', component: CadastroComponent},
-  {path: 'menu', component: MenuComponent},
-  {path: 'rodape', component: RodapeComponent}
+  {path: 'cadastro', component: CadastroComponent}
 
 ];
 
